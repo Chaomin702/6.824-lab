693517536a9956635183e34537eb9f94309d594a
Chaomin Wang <chaomin702@gmail.com>
2016-12-02 17:26:58 +0800

    Lab1 Part 3 completed

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -24,5 +24,38 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	nworks := len(mr.workers)
+	if phase == mapPhase {
+		for i := 0; i < nworks; i++ {
+			fmt.Println("work name ", <-mr.registerChannel)
+		}
+	}
+	taskBelongs := make([][]int, nworks)
+	for i := 0; i < ntasks; i++ {
+		taskBelongs[i%nworks] = append(taskBelongs[i%nworks], i)
+	}
+	chs := make([]chan int, nworks)
+
+	f := func(srv string, tasks []int, ch chan int) {
+		for _, v := range tasks {
+			taskArgs := new(DoTaskArgs)
+			taskArgs.File = mr.files[v]
+			taskArgs.JobName = mr.jobName
+			taskArgs.NumOtherPhase = nios
+			taskArgs.Phase = phase
+			taskArgs.TaskNumber = v
+			call(srv, "Worker.DoTask", &taskArgs, new(struct{}))
+		}
+		ch <- 1
+	}
+	for i := 0; i < nworks; i++ {
+		chs[i] = make(chan int)
+		go f(mr.workers[i], taskBelongs[i], chs[i])
+	}
+	for _, ch := range chs {
+		<-ch
+	}
+	//taskArgs := new(DoTaskArgs)
+
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
